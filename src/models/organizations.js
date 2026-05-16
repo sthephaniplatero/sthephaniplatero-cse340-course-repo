4cb85b9c17f2617a31c50029219933fbf4cdfe25
Sthephani Platero <platerosthephani@gmail.com>
2026-05-16 15:44:06 -0600

    update img

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -2,7 +2,7 @@ import db from './db.js'
 
 const getAllOrganizations = async () => {
     const query = `
-        SELECT organization_id, name, description, contact_email, logo_filename
+        SELECT organization_id, name, description, contact_email,
         FROM public.organizations;
     `;
 
